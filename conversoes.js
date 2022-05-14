3cfe9e68e6eec98d587223a8c7d7c453d4cb773b
Diego Borgheti <borgheti.d@gmail.com>
2022-05-14 14:46:25 -0300

    Codigos var e conversões

---
+++ conversoes.js
@@ -0,0 +1,18 @@
+// Tipo de dado 
+// Booleanos 
+
+// Conversão implicita 
+
+const numero = 456
+const numeroString = "456"
+
+
+
+// Number()
+// String()
+
+console.log(numero + Number(numeroString))
+
+
+
+// Conversão explicita 
---
+++ truthy-falsy.js
@@ -0,0 +1,33 @@
+// BOOLEAN 
+
+let usuarioLogado = true
+let contaPaga = false 
+
+/* TRUTHY OU FALSE 
+
+ 0 => false 
+ 1 => true 
+
+ */
+
+ console.log(0 == false)
+ console.log("" == false)
+ console.log(1 == true)
+
+// NULL E UNDEFINED 
+
+// Undefined => Quando a variável não é inicializada
+
+
+let minhaVAr; 
+let varNull = null; 
+
+let numero = 3; 
+let texto = "Alura"
+
+console.log("")
+console.log(typeof numero)
+console.log(typeof texto)
+console.log("")
+console.log(typeof minhaVAr)
+console.log(typeof varNull)
